a2c85a05efe7e9613d6e6a20a20f2de0668a407b
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-20 11:27:22 -0400

    Create Navbar file and navbar object for export

---
+++ src/modules/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function MyNavbar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand href="/">Noah Dragoon's Capstone Frontend</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/movies">Movies</Nav.Link>
+            <Nav.Link href="/actors">Actors</Nav.Link>
+            <Nav.Link href="/profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
