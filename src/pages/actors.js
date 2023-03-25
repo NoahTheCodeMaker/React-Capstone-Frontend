098a5a9d60707657802ff5f4b2e26750205180f2
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-25 05:20:17 -0400

    Update actors and movie pages to use new APICall.

--- src/pages/actors.js
+++ src/pages/actors.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import APICall from '../modules/tokens'
 
 function Actors() {
   return (
     <Container>
         <h1 className='top-spacer'>Actors</h1>
-        <Row className='mb-4'>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: 'green' }}>Create</div>
-            </Col>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: '#F6F1F1' }}>
-                    <APICall address="/actors" buttonText='Get Actors'/>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: 'blue' }}>Update</div>
-            </Col>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: 'red' }}>Delete</div>
-            </Col>
-        </Row>
+        <div style={{ height: '300px', backgroundColor: '#F6F1F1' }}>
+            <APICall address="/actors" buttonText='Get Actors'/>
+        </div>
     </Container>
   );
 }
--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import APICall from '../modules/tokens';
 
 function Movies() {
   return (
     <Container>
         <h1 className='top-spacer'>Movies</h1>
-        <Row className='mb-4'>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: 'green' }}>Create</div>
-            </Col>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: '#F6F1F1' }}>
-                    <APICall address="/movies" buttonText='Get Movies'/>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: 'blue' }}>Update</div>
-            </Col>
-            <Col md={6}>
-                <div style={{ height: '300px', backgroundColor: 'red' }}>Delete</div>
-            </Col>
-        </Row>
+        <div style={{ height: '300px', backgroundColor: '#F6F1F1' }}>
+            <APICall address="/movies" buttonText='Get Movies'/>
+        </div>
     </Container>
   );
 }
