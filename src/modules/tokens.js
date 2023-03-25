14986236c3d6c5eca5e6fcaa1669a75f1e519e1a
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-25 05:19:22 -0400

    Update APICall to automatically display all data.

--- src/modules/tokens.js
+++ src/modules/tokens.js
@@ -1,64 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button, Container, ListGroup } from 'react-bootstrap';
+import { Container, ListGroup, Spinner } from 'react-bootstrap';
 import axios from 'axios';
-
-const ActorList = ({ actors }) => (
-  <Container>
-    <ListGroup>
-      {actors.map(actor => (
-        <ListGroup.Item key={actor.id}>
-          {actor.name}, {actor.age}, {actor.gender}
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
-  </Container>
-);
-
-const MovieList = ({ movies }) => (
-  <Container>
-    <ListGroup>
-      {movies.map(movie => (
-        <ListGroup.Item key={movie.id}>
-          {movie.title}, {movie.release_date}
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
-  </Container>
-);
 
 const APICall = (props) => {
   const { getAccessTokenSilently } = useAuth0();
   const [actors, setActors] = useState([]);
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const callApi = async () => {
-    try {
-      const accessToken = await getAccessTokenSilently({
-        audience: 'https://NoahCapstone'
-      });
-      const response = await axios.get(`http://127.0.0.1:5000${props.address}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
+  useEffect(() => {
+    const callApi = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: 'https://NoahCapstone'
+        });
+        const response = await axios.get(`http://127.0.0.1:5000${props.address}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          }
+        });
+        if (props.address === '/actors') {
+          setActors(response.data.actors);
+        } else if (props.address === '/movies') {
+          setMovies(response.data.movies);
         }
-      });
-      if(props.address === '/actors'){
-        setActors(response.data.actors);
+        setIsLoading(false);
+      } catch (error) {
+        alert("You are not logged in!")
+        console.error(error);
       }
-      else if(props.address === '/movies'){
-        setMovies(response.data.movies);
-      }
-    } catch (error) {
-      alert("You are not logged in!")
-      console.error(error);
-    }
-  };
+    };
+    callApi();
+  }, [getAccessTokenSilently, props.address]);
+
+  if (isLoading) {
+    return (
+      <Container>
+        <span className="sr-only">Loading...</span>
+        <Spinner animation="border" role="status"/>
+      </Container>
+    );
+  }
 
   return (
     <Container>
-      <Button onClick={callApi}>{props.buttonText}</Button>
-      {props.address === '/actors' && <ActorList actors={actors} />}
-      {props.address === '/movies' && <MovieList movies={movies} />}
+      <ListGroup>
+        {props.address === '/actors' && actors.map(actor => (
+          <ListGroup.Item key={actor.id}>
+            {actor.name}, {actor.age}, {actor.gender}
+          </ListGroup.Item>
+        ))}
+        {props.address === '/movies' && movies.map(movie => (
+          <ListGroup.Item key={movie.id}>
+            {movie.title}, {movie.release_date}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </Container>
   );
 };
