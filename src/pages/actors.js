4e0b1470751331e5da1ad930eb71a86f77b5152a
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-27 10:46:07 -0400

    Add create buttons to actor and movie pages.

--- src/pages/actors.js
+++ src/pages/actors.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import APICall from '../modules/tokens'
 
 function Actors() {
+  const navigate = useNavigate();
+
+  const handleCreateActor = () => {
+    navigate('/actors/create');
+  };
+
   return (
     <Container>
         <h1 className='top-spacer'>Actors</h1>
+        <div className="d-flex justify-content-end mb-3">
+          <Button variant="success" onClick={handleCreateActor}>Create Actor</Button>
+        </div>
         <div style={{ height: '300px', backgroundColor: '#F6F1F1' }}>
-            <APICall address="/actors" buttonText='Get Actors'/>
+            <APICall address="/actors"/>
         </div>
     </Container>
   );
--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import APICall from '../modules/tokens';
 
 function Movies() {
+  const navigate = useNavigate();
+
+  const handleCreateMovie = () => {
+    navigate('/movies/create');
+  };
+
   return (
     <Container>
         <h1 className='top-spacer'>Movies</h1>
+        <div className="d-flex justify-content-end mb-3">
+          <Button variant="success" onClick={handleCreateMovie}>Create Movie</Button>
+        </div>
         <div style={{ height: '300px', backgroundColor: '#F6F1F1' }}>
-            <APICall address="/movies" buttonText='Get Movies'/>
+            <APICall address="/movies"/>
         </div>
     </Container>
   );
