8744015b5064391c93742b0826cf90ba52a299a9
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-26 21:33:06 -0400

    Add edit pages for actors and movies

---
+++ src/pages/editactor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function EditActor() {
+  return (
+    <Container>
+        <p>Edit Actors here.</p>
+    </Container>
+  );
+}
+
+export default EditActor;
---
+++ src/pages/editmovie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function EditMovie() {
+  return (
+    <Container>
+        <p>Edit Movies here.</p>
+    </Container>
+  );
+}
+
+export default EditMovie;
