b10626585f7d5659dc05458f82fc1088c953c92a
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-27 12:35:40 -0400

    Update home page with contact infor for fun.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,9 @@ import { Container } from "react-bootstrap";
 
 const HomePage = () => {
     return <Container>
-        <h1 className="top-spacer entry">Welcome to THE FRONTEND!</h1>
+        <h1 className="top-spacer entry">Welcome to the Frontend for the Udacity FSND Exam!</h1>
+        <h2 className="top-spacer entry">Visit my LinkedIn <a href="https://www.linkedin.com/in/noahdragoon">Here</a></h2>
+        <h2 className="top-spacer entry">Visit my GitHub <a href="https://github.com/NoahTheCodeMaker">Here</a></h2>
     </Container>
 };
 
