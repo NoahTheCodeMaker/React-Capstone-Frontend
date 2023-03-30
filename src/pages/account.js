1a0c71c7b70859369b141d14a1f61ecc96921127
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-30 16:44:23 -0400

    Remove unnecessary clutter in modules.

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container, Spinner } from "react-bootstrap";
-import LoginButton from "../modules/loginbutton";
-import LogoutButton from "../modules/logoutbutton";
+import { Container, Spinner, Button } from "react-bootstrap";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <Button variant="success" style={{marginTop: "30px"}} onClick={() => loginWithRedirect()}>Log In</Button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <Button variant="danger" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </Button>
+  );
+};
 
 const Profile = () => {
   const { user } = useAuth0();
