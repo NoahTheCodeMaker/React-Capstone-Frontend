3d6f471ba9d1676e20b809a0c63af1df836359df
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-30 19:37:19 -0400

    Add empty line to the end of the files.

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,8 @@ import { Container } from "react-bootstrap";
 const HomePage = () => {
     return <Container>
         <h1 className="top-spacer entry">Welcome to the Frontend for the Udacity FSND Exam!</h1>
+        <h2 className="entry">Please select the Account page from the NavBar above to get started.</h2>
+        <h2 className="entry">Don't forget to remove the permissions in the backend if you want to test the endpoints!</h2>
         <h2 className="top-spacer entry">Visit my LinkedIn <a href="https://www.linkedin.com/in/noahdragoon">Here</a></h2>
         <h2 className="top-spacer entry">Visit my GitHub <a href="https://github.com/NoahTheCodeMaker">Here</a></h2>
     </Container>
